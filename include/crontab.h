89e8a04d5b9741f92dcf33fc132fb630d3f1f8fa
PfeifferP <admin@pfeiffer-privat.de>
2023-01-29 16:18:08 +0100

    neue Änderungen

--- include/crontab.h
+++ include/crontab.h
@@ -7,10 +7,10 @@ int cron_1 = 1;  //5 Seconds intervall for timer 1;
 int cron_2 = 5; //20 Seconds intervall for timer 2;
 int cron_3 = 30; //30 Seconds intervall for timer 3;
 int cron_4 = 60;
-int cron_5 = 300;
+int cron_5 = 60;
 //////////////////////// End Timer Settings ////////////////////////////////////////////
 long cront1, cront2, cront3, cront4, cront5;
-
+bool cron1_e, cron2_e, cron3_e, cron4_e, cron5_e = 0;
 
 
 
@@ -30,7 +30,7 @@ void cron1() {
   }
   
   switch (actpage) {
-  case 1:
+  case 1: 
     tft.drawCentreString("Page 1",160,120,2);
     break;
   case 2:
@@ -63,15 +63,25 @@ void cron4() {
 
 void cron5() {
   //Your Cronjob action here
+  for (int i = 0; i <= 210; i++){
+    ledcWrite(0,i);
+    delay(10);
+  }
   
+  cron5_e = 0 ;
 }
 
 void cronjob() {
   long tmp = millis();
-  if ((cront1 + (cron_1 * 1000)) <= tmp) { cron1(); cront1 = millis(); }
-  if ((cront2 + (cron_2 * 1000)) <= tmp) { cron2(); cront2 = millis(); }
-  if ((cront3 + (cron_3 * 1000)) <= tmp) { cron3(); cront3 = millis(); }
-  if ((cront4 + (cron_4 * 1000)) <= tmp) { cron4(); cront4 = millis(); }
-  if ((cront5 + (cron_5 * 1000)) <= tmp) { cron5(); cront5 = millis(); }
+  if ((cron1_e == 0)) {cront1 = millis();}
+  if ((cron1_e == 1) && (cront1 + (cron_1 * 1000)) <= tmp) { cron1(); cront1 = millis(); }
+  if ((cron2_e == 0)) {cront2 = millis();}
+  if ((cron2_e == 1) && (cront2 + (cron_2 * 1000)) <= tmp) { cron2(); cront2 = millis(); }
+  if ((cron3_e == 0)) {cront3 = millis();}
+  if ((cron3_e == 1) && (cront3 + (cron_3 * 1000)) <= tmp) { cron3(); cront3 = millis(); }
+  if ((cron4_e == 0)) {cront4 = millis();}
+  if ((cron4_e == 1) && (cront4 + (cron_4 * 1000)) <= tmp) { cron4(); cront4 = millis(); }
+  if ((cron5_e == 0)) {cront5 = millis();}
+  if ((cron5_e == 1) && (cront5 + (cron_5 * 1000)) <= tmp) { cron5(); cront5 = millis(); }
 }
 
